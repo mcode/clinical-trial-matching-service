00f4a40e51d4a8708bcb26c6f7f14d246a603383
Lister, Zach (ZLISTER) <zlister@mitre.org>
2021-04-30 09:47:22 -0400

    tabs to spaces

--- scripts/loader.py
+++ scripts/loader.py
@@ -22,21 +22,21 @@ parameter = {"resource":
 
 # Loop through each record
 for record in records:
-	print(record)
-	with open(record) as f:
-		data = json.load(f)
+  print(record)
+  with open(record) as f:
+    data = json.load(f)
 
-		# Add the parameter resource to the record
-		data["entry"].append(parameter)
-		data["type"] = "collection"
+    # Add the parameter resource to the record
+    data["entry"].append(parameter)
+    data["type"] = "collection"
 
-		# Send the patient bundle to the wrapper
-		response = requests.post('http://localhost:3000/getClinicalTrial', data=json.dumps(data), headers={"Content-Type":"application/json"})
-		researchStudies = response.json()
+    # Send the patient bundle to the wrapper
+    response = requests.post('http://localhost:3000/getClinicalTrial', data=json.dumps(data), headers={"Content-Type":"application/json"})
+    researchStudies = response.json()
 
-		# Create a .csv file in the resultsDirectory and write the NCTID of each match to that file 
-		if (researchStudies["total"] > 0):
-			with open(resultsDirectory + "/" + record[:-5] + ".csv", mode='w') as result_file:
-				writer = csv.writer(result_file)
-				for entry in researchStudies["entry"]:
-					writer.writerow([entry["resource"]["id"]])
+    # Create a .csv file in the resultsDirectory and write the NCTID of each match to that file 
+    if (researchStudies["total"] > 0):
+      with open(resultsDirectory + "/" + record[:-5] + ".csv", mode='w') as result_file:
+        writer = csv.writer(result_file)
+        for entry in researchStudies["entry"]:
+          writer.writerow([entry["resource"]["id"]])
