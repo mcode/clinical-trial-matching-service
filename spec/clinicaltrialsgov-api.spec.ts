06add65e507215b2f4e74b386a327d8fc309e9d5
Daniel Potter <dpotter@mitre.org>
2023-10-11 17:01:41 -0400

    Increase test coverage

--- spec/clinicaltrialsgov-api.spec.ts
+++ spec/clinicaltrialsgov-api.spec.ts
@@ -1,4 +1,5 @@
-import { ClinicalTrialsGovAPI, isPagedStudies, DEFAULT_ENDPOINT} from "../src/clinicaltrialsgov-api";
+import { ClinicalTrialsGovAPI, isPagedStudies, DEFAULT_ENDPOINT, Study } from '../src/clinicaltrialsgov-api';
+import * as nock from 'nock';
 
 describe('.isPagedStudes', () => {
   it('rejects invalid objects', () => {
@@ -6,7 +7,7 @@ describe('.isPagedStudes', () => {
     expect(isPagedStudies(null)).toBeFalse();
     expect(isPagedStudies({})).toBeFalse();
     expect(isPagedStudies({ studies: true })).toBeFalse();
-    expect(isPagedStudies({ studies: [ 'invalid' ]})).toBeFalse();
+    expect(isPagedStudies({ studies: ['invalid'] })).toBeFalse();
   });
 });
 
@@ -17,9 +18,53 @@ describe('ClinicalTrialsGovAPI', () => {
       expect(instance['_endpoint']).toEqual(DEFAULT_ENDPOINT);
       expect(typeof instance['_log']).toEqual('function');
     });
+
+    it('uses the defaults if given an empty option element', () => {
+      const instance = new ClinicalTrialsGovAPI({});
+      expect(instance['_endpoint']).toEqual(DEFAULT_ENDPOINT);
+      expect(typeof instance['_log']).toEqual('function');
+    });
   });
 
   describe('#fetchStudies', () => {
-    it('raises an error if the result does not return JSON', () => {});
+    let scope: nock.Scope;
+    let interceptor: nock.Interceptor;
+    let api: ClinicalTrialsGovAPI;
+
+    beforeEach(() => {
+      scope = nock('https://clinicaltrials.gov');
+      interceptor = scope.get('/api/v2/studies?filter.ids=NCT12345678');
+      api = new ClinicalTrialsGovAPI({ endpoint: 'https://clinicaltrials.gov/api/v2' });
+    });
+
+    it('raises an error if the result does not return JSON', async () => {
+      interceptor.reply(200, "something that isn't JSON");
+      await expectAsync(api.fetchStudies(['NCT12345678'])).toBeRejected();
+    });
+
+    it("raises an error if given a JSON response it doesn't understand", async () => {
+      interceptor.reply(200, '{"nothing":"valid"}', { 'Content-Type': 'application/json' });
+      await expectAsync(api.fetchStudies(['NCT12345678'])).toBeRejectedWithError(
+        'Server returned a success response, but the result could not be parsed.'
+      );
+    });
+
+    it('raises an error on server errors', async () => {
+      interceptor.reply(500, 'Server on fire');
+      await expectAsync(api.fetchStudies(['NCT12345678'])).toBeRejected();
+    });
+
+    it('returns studies', async () => {
+      const testStudy: Study = {
+        protocolSection: {
+          identificationModule: {
+            nctId: 'NCT12345678'
+          }
+        }
+      };
+      interceptor.reply(200, JSON.stringify({ studies: [testStudy] }), { 'Content-Type': 'application/json' });
+      const studies = await api.fetchStudies(['NCT12345678']);
+      expect(studies).toEqual([testStudy]);
+    });
   });
 });
